67c13286ae01ddbf078dfdc54b88f94aca10ef8e
firlandofarel27 <firlandofarel27@gmail.com>
2026-05-22 09:15:57 +0700

    Pesanan

---
+++ pertemuan11/pesanan.java
@@ -0,0 +1,13 @@
+package pertemuan11;
+
+public class pesanan {
+    int kodepesanan;
+    String namapesanan;
+    int harga;
+
+    pesanan(int kodepesanan, String namapesanan, int harga) {
+        this.kodepesanan = kodepesanan;
+        this.namapesanan = namapesanan;
+        this.harga = harga;
+    }
+}
