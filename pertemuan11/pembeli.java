e55eaf5702fec170ffd283a037c4f28b4ecaf8b6
firlandofarel27 <firlandofarel27@gmail.com>
2026-05-22 09:15:30 +0700

    Pembeli

---
+++ pertemuan11/pembeli.java
@@ -0,0 +1,12 @@
+package pertemuan11;
+
+public class pembeli {
+    String namapembeli;
+    String noHp;
+
+    pembeli(String namapembeli, String noHp) {
+        this.namapembeli = namapembeli;
+        this.noHp = noHp;
+    }
+}
+
