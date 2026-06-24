4c48044ea92f7ce95eae0558336d390e6eaea55a
firlandofarel27 <firlandofarel27@gmail.com>
2026-06-24 10:14:09 +0700

    Node Lomba

---
+++ jobsheet14/Node.java
@@ -0,0 +1,11 @@
+package jobsheet14;
+
+public class Node {
+    PesertaLomba data;
+    Node left, right;
+
+    public Node(PesertaLomba data) {
+        this.data = data;
+        left = right = null;
+    }
+}
